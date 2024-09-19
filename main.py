8bf2b966d9313c5de9521b91f3c95c329061ebe7
arefvandadi <aref.vandadi@gmail.com>
2024-09-19 13:28:41 -0400

    Define function to remove coutires with less datapoints than a threshold. Update the data with the function

--- main.py
+++ main.py
@@ -31,5 +31,27 @@ data_df.head(10)
 # let's only focus on full-time employment 
 # and we can drop the employment column after
 data_df = data_df[data_df["Employment"]=="Employed full-time"]
+data_df.head(10)
+data_df.drop("Employment", axis=1, inplace=True)
 
+# Data Per Country
+data_df["Country"].value_counts()
+data_df["Country"].value_counts().index
+data_df["Country"].value_counts().values
 
+# Defined a function to remove all countries with less data than a cutoff value
+def filter_countries_per_occurence(data_df:pd.DataFrame, cutoff: int) -> pd.DataFrame:
+    """
+    Removes countries with number of data points below cutoff
+    Parameters:
+        cutoff (int): number of datapoint for countries.
+    """
+    selected_countries = [data_df["Country"].value_counts().index[i] for i, j in enumerate(data_df["Country"].value_counts().values) if j>cutoff]
+    data_df = data_df[data_df["Country"].isin(selected_countries)]
+    return data_df
+
+# Use the above function to remove all countries with smaller than 400 datapoints.
+data_df = filter_countries_per_occurence(data_df, 400)
+
+#Check if countries with low datapoints were removed
+data_df["Country"].value_counts()
