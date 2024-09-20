eebf64ba6fcc8f53d22c876164daf4c10aae272b
arefvandadi <aref.vandadi@gmail.com>
2024-09-20 09:06:52 -0400

    Update prediction page with selectors for country and education level and slider for experiece and calculate salary button.

--- predict_page.py
+++ predict_page.py
@@ -10,3 +10,47 @@ def load_model():
 
 model, le_Country, le_EdLevel = load_model()
 
+
+def show_predict_page():
+    st.title("Software Developer Salary Prediction")
+
+    st.write("""### We need some information to predict the salary""")
+
+    countries = (
+        "United States",
+        "India",    
+        "United Kingdom",    
+        "Germany",   
+        "Canada",          
+        "Brazil",           
+        "France",           
+        "Spain",           
+        "Australia",         
+        "Netherlands",       
+        "Poland",      
+        "Italy", 
+        "Russian Federation",
+        "Sweden",
+    )
+
+    education = (
+        "Less than a Bachelors",
+        "Bachelor's degree",
+        "Master's degree",
+        "Post grad",
+    )
+
+    country = st.selectbox("Country", countries)
+    education_level = st.selectbox("Education", education)
+
+    experience = st.slider("Years of Experience", 0, 50, 3, 1)
+
+    salary_button_clicked = st.button("Calculate Salary")
+
+    if salary_button_clicked:
+        input = np.array([[country, education_level, experience]])
+        input[:, 0] = le_Country.fit_transform(input[:, 0])
+        input[:, 1] = le_EdLevel.fit_transform(input[:, 1])
+        input = input.astype(float)
+        predicted_salary = model.predict(input)
+        st.subheader(f"The estimated salary is ${predicted_salary[0]:.2f}")
